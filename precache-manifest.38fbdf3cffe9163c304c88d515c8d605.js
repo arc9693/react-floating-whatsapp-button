8963b5a93ebe131f2996fba73bfd5f07caa4499d
arc9693 <archana.choudhary.9693@gmail.com>
2021-06-11 09:50:16 +0530

    Updates

---
+++ precache-manifest.38fbdf3cffe9163c304c88d515c8d605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75bb3350e23e753f8297079b0286afee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ff667a8374f6ebc8f898",
+    "url": "./static/css/main.353624b4.chunk.css"
+  },
+  {
+    "revision": "7c40129fb5f7a5edd78b",
+    "url": "./static/js/2.f85dbf43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f85dbf43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff667a8374f6ebc8f898",
+    "url": "./static/js/main.ff17a01b.chunk.js"
+  },
+  {
+    "revision": "4164d23a508b81770b74",
+    "url": "./static/js/runtime-main.39e0e5c1.js"
+  }
+]);
